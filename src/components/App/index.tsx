643be90b4f8a86bca93821a1632df84f9096c927
Marta Zazlak <marta.zazlak@hotmail.com>
2021-04-07 19:24:57 +0200

    feat: initial (eslint, prettier, cra, structure)

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+const App = () => (
+    <div className="App">
+        <header className="App-header">
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+        </header>
+        <main>main</main>
+        <footer>footer</footer>
+    </div>
+);
+
+export default App;
