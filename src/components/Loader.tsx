2f0e6a031e49e14d45c761f8e37877472a997c67
Marta Zazlak <marta.zazlak@hotmail.com>
2021-04-13 11:23:16 +0200

    feat: add UserInfo tests

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,3 +1,3 @@
-const Loader = () => <div>Loading...</div>;
+const Loader = () => <div data-testid="loader">Loading...</div>;
 
 export default Loader;
---
+++ src/tests/UserInfo.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import * as ApiHelpers from '../api/helpers';
+import { IUser } from '../api/interfaces';
+import UserInfo from '../page/Main/UserInfo';
+
+const USER: IUser = {
+    name: 'Mairon',
+    bio: 'bio bio bio here',
+    avatar_url: 'http://avatars/avatar.jpeg',
+    public_repos: 1,
+};
+
+const setup = () => {
+    const queryClient = new QueryClient({
+        defaultOptions: { queries: { retry: false } },
+    });
+
+    render(
+        <QueryClientProvider client={queryClient}>
+            <UserInfo username={USER.name} />
+        </QueryClientProvider>
+    );
+};
+
+describe('UserInfo', () => {
+    test('Should render all elements, when data is ready', async () => {
+        // @ts-ignore
+        ApiHelpers.fetchUserData = jest.fn().mockResolvedValue(USER);
+        setup();
+
+        const name = await screen.findByText(USER.name);
+        const avatar = await screen.findByAltText('Selected user avatar');
+        const bio = await screen.findByText(USER.bio);
+
+        expect(name).toBeInTheDocument();
+        expect(avatar).toBeInTheDocument();
+        expect(bio).toBeInTheDocument();
+    });
+
+    test('Should render loader', () => {
+        // @ts-ignore
+        ApiHelpers.fetchUserData = jest
+            .fn()
+            .mockResolvedValue({ isLoading: true });
+        setup();
+
+        expect(screen.getByTestId('loader')).toBeInTheDocument();
+    });
+});
